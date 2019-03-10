b62e80de8def0b4a780264da16eea2e67ce51697
Levin Winter <33220502+levinwinter@users.noreply.github.com>
2019-03-10 16:19:09 +0100

    Hello World Button with console out

---
+++ src/de/levinwinter/Controller.java
@@ -0,0 +1,28 @@
+package de.levinwinter;
+
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Controller {
+
+    public void goClicked() throws IOException {
+
+        //creating a new empty slide show
+        //XMLSlideShow ppt = new XMLSlideShow();
+
+        //creating an FileOutputStream object
+        //File file = new File("example1.pptx");
+        //FileOutputStream out = new FileOutputStream(file);
+
+        //saving the changes to a file
+        //ppt.write(out);
+        //System.out.println("Presentation created successfully");
+        //out.close();
+
+        System.out.println("clicked!");
+
+    }
+}
--- src/de/levinwinter/Main.java
+++ src/de/levinwinter/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package de.levinwinter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
